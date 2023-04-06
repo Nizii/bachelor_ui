0c303297c6cc4f46bead24ca2cc4ced042890e30
Nizam <nizam.oezdemir@gmx.ch>
2023-04-06 11:15:30 +0200

    changed api from local to azure ser

--- pages/Undecided.vue
+++ pages/Undecided.vue
@@ -162,8 +162,8 @@
     },
     async created() {
         try {
-            //const response = await axios.get('https://interactivemenu.azurewebsites.net/api/wine');
-            const response = await axios.get('https://localhost:44322/api/Wine');
+            const response = await axios.get('https://interactivemenu.azurewebsites.net/api/wine');
+            //const response = await axios.get('https://localhost:44322/api/Wine');
             this.wines = response.data;
             this.loading = false;
         } catch (error) {
