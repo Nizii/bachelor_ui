1fa7c959be9ce4386b3a8e3efe99a0d11ddaef76
Nizii <nizam.oezdemir@gmx.ch>
2023-05-28 15:53:36 +0200

    URL für Flaggen Aufruf angepasst

--- components/Tabbars/NationTabbar.vue
+++ components/Tabbars/NationTabbar.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="button-group">
     <button @click="showWinesForNation('Schweiz')" class="button button-1">Schweiz
-      <img :src="require('static/icons/nations/schweiz.png')" class="icon" alt="Profile icon" />
+      <img :src="require('static/icons/nations/Schweiz.png')" class="icon" alt="Profile icon" />
     </button>
     <button @click="showWinesForNation('Italien')" class="button button-2">Italien
-    <img :src="require('static/icons/nations/italien.png')" class="icon" alt="Profile icon" />
+    <img :src="require('static/icons/nations/Italien.png')" class="icon" alt="Profile icon" />
       </button>
     <button @click="showWinesForNation('Frankreich')" class="button button-3">Frankreich
-      <img :src="require('static/icons/nations/frankreich.png')" class="icon" alt="Profile icon" />
+      <img :src="require('static/icons/nations/Frankreich.png')" class="icon" alt="Profile icon" />
     </button>
     <button @click="showWinesForNation('Spanien')" class="button button-3">Spanien
-      <img :src="require('static/icons/nations/spanien.png')" class="icon" alt="Profile icon" />
+      <img :src="require('static/icons/nations/Spanien.png')" class="icon" alt="Profile icon" />
     </button>
   </div>
 </template>
