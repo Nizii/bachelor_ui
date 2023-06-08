5830af0465ec7c2756a02963ae8b72f40ff1c731
Nizam <34688063+Nizii@users.noreply.github.com>
2023-06-08 22:37:21 +0000

    csss angepasst

--- components/ProfileComponent/CellarItem.vue
+++ components/ProfileComponent/CellarItem.vue
@@ -25,16 +25,18 @@
 .wine-item {
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   height: 260px; 
 }
 
 .item-title {
-  margin-bottom: 10px; 
+  
 }
 
 .wine-image-profile {
   width: 50px;
   object-fit: cover;
+  margin-bottom: 0; 
 }
 </style>
